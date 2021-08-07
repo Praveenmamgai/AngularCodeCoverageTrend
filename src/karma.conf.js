4e4bbc4ecb4db8424f1c919ca041c336c03a0791
Praveenmamgai <57629685+Praveenmamgai@users.noreply.github.com>
2021-08-07 20:31:17 +0530

    Update karma.conf.js

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -5,10 +5,18 @@ module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    files: [
+      'src/**/*.js',
+      'test/**/*.js'
+  ],
+  preprocessors: {
+    'src/**/*.js': ['coverage']
+},
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
       require('karma-coverage-istanbul-reporter'),
       require('karma-junit-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -19,11 +27,18 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage/angular-unit-testing'),
-      reports: ['html', 'json', 'lcovonly', 'text-summary'],
+      reports: ['cobertura', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
     },
-
-    reporters: ['progress', 'kjhtml', 'junit'],
+    coverageReporter: {
+      includeAllSources: true,
+      dir: require('path').join(__dirname, '..report/coverage/'),
+      reporters: [
+          { type: "cobertura", subdir: ".", file: 'cobertura.xml' },
+          { type: 'text-summary' }
+      ]
+  },
+    reporters: ['progress', 'kjhtml', 'junit', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
